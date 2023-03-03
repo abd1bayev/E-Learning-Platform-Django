df75cc976c130896894ee186b068447aefaa031f
student <jasurabdibayev0@gmail.com>
2023-03-03 11:19:21 +0500

    course update

--- educa/educa/urls.py
+++ educa/educa/urls.py
@@ -3,6 +3,7 @@ from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib.auth import views as auth_views
+from courses.views import CourseListView
 
 
 urlpatterns = [
@@ -12,6 +13,7 @@ urlpatterns = [
           name='logout'),
     path('admin/', admin.site.urls),
     path('course/', include('courses.urls')),
+    path('', CourseListView.as_view(), name='course_list'),
 ]
 
 if settings.DEBUG:
